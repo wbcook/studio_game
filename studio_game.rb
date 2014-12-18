d1c8c4b4a3ad9c141946cb67bffd4b5ee03a7e2c
William Cook <wbcook@users.noreply.github.com>
2014-12-18 17:44:12 -0500

    Create studio_game.rb

---
+++ studio_game.rb
@@ -0,0 +1,22 @@
+# Encapsulating behaviors and states of larry, curly, and moe
+# using classes, also the methods: to_s, and initialize.
+require_relative 'player'
+require_relative 'game'
+# Output goes here...
+larry = Player.new "larry"
+moe = Player.new "moe", 60
+curly = Player.new "curly", 125
+
+knuckleheads = Game.new("Knuckleheads")
+knuckleheads.add_player(larry)
+knuckleheads.add_player(moe)
+knuckleheads.add_player(curly)
+knuckleheads.play
+
+chipmunks = Game.new("Chipmunks")
+chipmunks.add_player(Player.new "alvin", 50)
+chipmunks.add_player(Player.new "simon", 80)
+chipmunks.add_player(Player.new "theodore", 130)
+chipmunks.play
+
+# EOF :)
