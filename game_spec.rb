6a1a3bd84da6bfe87a8e15c877b10aba436a1ced
William Cook <wbcook@users.noreply.github.com>
2014-12-31 17:26:19 -0500

    Create game_spec.rb

---
+++ game_spec.rb
@@ -0,0 +1,35 @@
+# Run tests with rspec game_spec.rb --color
+# Run ALL _spec.rb tests in the directory with rspec . -- color
+# This tests the player class.
+require_relative 'game'
+
+describe Game do
+
+  before do
+    @game = Game.new("Knuckleheads")
+
+    @initial_health = 100
+    @player = Player.new("moe", @initial_health)
+
+    @game.add_player(@player)
+  end
+  it "should w00t the player when a high number is rolled" do
+    Die.any_instance.stub(:roll).and_return(5)
+
+    @game.play
+    @player.health.should == @initial_health + 15
+  end
+  it "should do nothing to the player when a medium number is rolled" do
+    Die.any_instance.stub(:roll).and_return(3)
+
+    @game.play
+    @player.health.should == @initial_health
+  end
+  it "should blam the player when a low number is rolled" do
+    Die.any_instance.stub(:roll).and_return(1)
+
+    @game.play
+    @player.health.should == @initial_health - 10
+  end
+
+end
