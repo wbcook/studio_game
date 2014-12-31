96694ff0e3528c29078fcb74a33c86d5525e4b17
William Cook <wbcook@users.noreply.github.com>
2014-12-31 17:31:44 -0500

    Update die.rb

--- die.rb
+++ die.rb
@@ -1,7 +1,13 @@
 # Rolls high, medium or low dice rolls to determine a w00t or blam on a player.
 
 class Die
+  attr_reader :number
+  
+  def initialize
+    roll
+  end
+
   def roll
-    rand(1..6)
+    @number = rand(1..6)
   end
 end
