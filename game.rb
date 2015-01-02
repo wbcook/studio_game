b326445fb4d1f1b23f4b0699f88e0e3637a99a41
William Cook <wbcook@users.noreply.github.com>
2015-01-02 15:50:54 -0500

    Update game.rb

--- game.rb
+++ game.rb
@@ -1,7 +1,7 @@
 # Encapsulating behaviors and states of larry, curly, and moe
 # using classes, also the methods: to_s, and initialize.
 require_relative 'player'
-require_relative 'die'
+require_relative 'game_turn'
 class Game
   attr_reader :title
   def initialize(title)
@@ -15,15 +15,7 @@ class Game
     puts "There are #{@players.size} players in the game:"
     puts @players
     @players.each do |player|
-      die = Die.new
-      case die.roll
-      when 1..2
-         player.blam
-      when 3..4
-        puts "#{player.name} was skipped!"
-      else
-        player.w00t
-      end
+      GameTurn.take_turn(player)
     end
     puts @players
     puts "#{@title}".center(50, "*")
